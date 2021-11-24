4b30a781327017b0e3ea4bac9566d2da60813010
Tuo Zhang <taz2008@med.cornell.edu>
2021-11-24 14:24:56 -0500

    modify the rule 'star_align': running a python script instead of using the 'run' block; the latter one does not allow creating an conda environment

---
+++ scripts/star_align.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# star_align.py
+# align the trimmed reads to the reference genome using STAR
+# 
+
+import sys
+import subprocess
+import os.path
+from contextlib import redirect_stdout
+
+# sequencing type (default: single-end)
+seqtype = 'SE'
+
+# input fastq files
+fq_files = snakemake.input['read1']
+if 'read2' in snakemake.input:
+    seqtype = 'PE'
+    fq_files = '{} {}'.format(snakemake.input['read1'], snakemake.input['read2'])
+
+# wildcards.sample
+sample = snakemake.wildcards
+
+# output bam file
+bamfile = snakemake.output['bam']
+
+# output bam prefix
+bamprefix= bamfile.replace('Aligned.sortedByCoord.out.bam','')
+
+# output bam folder
+bamdir = os.path.dirname(bamfile)
+
+# library type
+libtype = snakemake.params['libtype']
+
+# indexed reference genome folder
+staridx = snakemake.config["staridx"]
+
+# parameters for STAR
+seedLmax = snakemake.config["seedLmax"]
+multimapNmax = snakemake.config["multimapNmax"]
+overhangMin = snakemake.config["overhangMin"]
+DBoverhangMin = snakemake.config["DBoverhangMin"]
+
+# threads
+threads = snakemake.threads
+
+# log file
+star_log_file = snakemake.log['bam']
+samtools_log_file = snakemake.log['bai']
+
+# compatibility for Cufflinks
+flag = ''
+if libtype == 'fr-unstranded':
+    flag = '--outSAMstrandField intronMotif'
+
+# run STAR to generate alignment
+star_command = 'STAR --runThreadN {} --genomeDir {} --readFilesIn {} --readFilesCommand zcat --outFileNamePrefix {} {} --outReadsUnmapped Fastx --outSAMtype BAM SortedByCoordinate --outFilterType BySJout --outFilterMultimapNmax {} --alignSJoverhangMin {} --alignSJDBoverhangMin {} --seedSearchStartLmax {} --twopassMode Basic >{} 2>&1'.format(threads,staridx,fq_files,bamprefix,flag,multimapNmax,overhangMin,DBoverhangMin,seedLmax,star_log_file)
+
+gzip_command = 'gzip {}/{}.Unmapped.out.mate*'.format(bamdir,sample)
+
+samtools_command = 'samtools index -@ {} {} >{} 2>&1'.format(threads,bamfile,samtools_log_file)
+
+# functions
+def run_command(command):
+    print(command)
+    res = subprocess.run(args=command, shell=True, capture_output=True, text=True)
+    print(res.stdout)
+    sys.stdout.flush()
+    print(res.stderr)
+    sys.stdout.flush()
+
+# run
+with gzip.open(htseq_log_file, 'wt') as flog:
+    with redirect_stdout(flog):
+        # run STAR
+        run_command(star_command)
+        # zip unmapped reads
+        run_command(gzip_command)
+        # index bam file
+        run_command(samtools_command)
+        print('Complete!')
+
